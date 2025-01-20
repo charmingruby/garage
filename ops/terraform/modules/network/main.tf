b78ddf64cb754b343dc403635da2ad10b28b511a
charmingruby <mrrubycanal3105@gmail.com>
2025-01-20 10:56:15 -0300

    chore: setup vpcs outputs

--- ops/terraform/modules/network/main.tf
+++ ops/terraform/modules/network/main.tf
@@ -1,14 +1,21 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  namespaced_department_name = var.namespaced_department_name
+  network                    = var.network
+}
+
 module "vpce" {
   count = var.create_vpc_endpoint ? 1 : 0
 
+  depends_on = [module.vpc]
+
   source = "./modules/vpce"
 
-
-  private_subnet_ids         = aws_subnet.private.*.id
-  vpc_name                   = format("%s-vpce", var.namespaced_department_name)
-  private_route_table_ids    = aws_route_table.private.*.id
-  cidr_block                 = aws_vpc.this.cidr_block
-  aws_region                 = var.aws_region
-  namespaced_department_name = var.namespaced_department_name
-  vpc_id                     = aws_vpc.this.id
+  private_subnet_ids      = module.vpc.subnets.private.id
+  vpc_name                = format("%s-vpce", var.namespaced_department_name)
+  vpc_id                  = module.vpc.vpc_config.id
+  private_route_table_ids = module.vpc.route_tables.private.id
+  cidr_block              = module.vpc.vpc_config.cidr_block
+  aws_region              = var.aws_region
 }
